634551276d42c9d6152bd8062092534300b7a262
claireporter26 <58444269+claireporter26@users.noreply.github.com>
2021-02-06 22:08:13 +0000

    working on rotate functionality

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const gameItems = document.querySelectorAll('.game-item');
+
+
+function rotateItem(item){
+    item.style.transform = "rotate(90deg)";
+   
+}
+
+
+// gameItems.forEach(function(item){
+
+//     item.addEventListener('click', rotateItem(e))
+//     // item.addEventListener('click', function(e){
+//     //     // let clickedItem = e.target;
+        
+
+//     //     // clickedItem.style.transform = "rotate(90deg)";
+//     // })
+// });
+
