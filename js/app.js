535727f5ae907923d24bf5a6ee970d25fe9474a0
claireporter26 <58444269+claireporter26@users.noreply.github.com>
2021-02-13 22:55:29 +0000

    started working on item animation

--- js/app.js
+++ js/app.js
@@ -1,22 +1,38 @@
 const gameItems = document.querySelectorAll('.game-item');
+const btnStartGame = document.getElementById('btn-start-game')
 
+
+btnStartGame.addEventListener('click', startGame)
 
 function rotateItem(item){
     // item.style.transform = "rotate(90deg)";
     console.log("clicked")
 
-    if(!item.classList.contains("rotate90")){
+    if (item.classList.contains("no-rotate")){
         item.classList.add("rotate90");
-    }else if(item.classList.contains("rotate90")){
+        item.classList.remove("no-rotate");
+    } else if(item.classList.contains("rotate90")){
         
         item.classList.add("rotate180");
         item.classList.remove("rotate90");
-    }else{
-        console.log("something else")
+    } else if (item.classList.contains("rotate180")){
+        item.classList.add("rotate270");
+        item.classList.remove("rotate180");
+    } else if (item.classList.contains("rotate270")){
+        item.classList.add("rotate360");
+        item.classList.remove("rotate270");
+    } else if (item.classList.contains("rotate360")){
+        item.classList.add("rotate90");
+        item.classList.remove("rotate360");
     }
     
    
 }
+
+function startGame() {
+    gameItems[0]
+}
+
 
 //might work
 
