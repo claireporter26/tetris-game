e466841c85076eb11c6790375585f66b35a22176
claireporter26 <58444269+claireporter26@users.noreply.github.com>
2021-02-13 11:49:50 +0000

    working on rotating items

--- js/app.js
+++ js/app.js
@@ -2,9 +2,30 @@ const gameItems = document.querySelectorAll('.game-item');
 
 
 function rotateItem(item){
-    item.style.transform = "rotate(90deg)";
+    // item.style.transform = "rotate(90deg)";
+    console.log("clicked")
+
+    if(!item.classList.contains("rotate90")){
+        item.classList.add("rotate90");
+    }else if(item.classList.contains("rotate90")){
+        
+        item.classList.add("rotate180");
+        item.classList.remove("rotate90");
+    }else{
+        console.log("something else")
+    }
+    
    
 }
+
+//might work
+
+// var eventValue = function (event) {
+//     document.body.appendChild(document.createElement('div'))
+//     .textContent = event.type;
+//  }
+//  var pressed = document.querySelector("#pressButtonDemo");
+//  pressed.addEventListener("click", eventValue);
 
 
 // gameItems.forEach(function(item){
